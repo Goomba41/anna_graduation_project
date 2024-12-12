5526671d5e99d6d9507f9444dfba27ef75ab1c4a
Антон Бородавкин <borodavkin@kn-k.ru>
2024-12-12 08:47:00 +0300

    Исправление типа аналитики

--- frontend/src/typings/analytics.types.ts
+++ frontend/src/typings/analytics.types.ts
@@ -2,7 +2,13 @@ import z from "zod";
 
 export const ZDoughnutData = z.object({
   labels: z.array(z.string()),
-  datasets: z.array(z.object({ data: z.array(z.number()) })),
+  datasets: z.array(
+    z.object({
+      data: z.array(z.number()),
+      type: z.string().nullish(),
+      label: z.string().nullish(),
+    }),
+  ),
 });
 
 export const ZAnalytic = z.record(z.string(), ZDoughnutData);
